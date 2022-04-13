6edd51ac121507b3d5cc3aaac238e1ae5faee2c6
shunsuke-chiba <hoshigame.297@gmail.com>
2022-04-13 06:45:38 +0000

    [Update3]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
   def create
     book =Book.new(book_params)
     book.save
-    redirect_to book_path(@book.id)
+    redirect_to book_path(book.id)
   end
 
   def show
@@ -34,6 +34,7 @@ class BooksController < ApplicationController
   end
   
   private
+  
   def book_params
     params.require(:book).permit(:title,:body)
   end
